b1547cfc2458bcc95a75b9f7262926318ff773ac
Anthos Makris <s2036401@ed.ac.uk>
2024-05-09 13:49:08 +0100

    Update activity_selector.sh
    add default ActRandomisation value

--- captures/Controlfunctions/activity_selector.sh
+++ captures/Controlfunctions/activity_selector.sh
@@ -2,7 +2,9 @@
                                                                                                                                                                                                                                                                        
 NScen=$1
 
-
+# if not set, set PWRandomisation to default = 1
+[ -z "$ActRandomisation" ] && ActRandomisation=1
+# generate number from 1 to NScen inclusive
 if [[ ${ActRandomisation} = "1" ]]; then
 echo "Activity randomisation active"
 	export SCENARIO=$((1 + RANDOM % ${NScen}))
@@ -14,4 +16,3 @@ export Description=${Descriptions[${SCENARIO}-1]}
 echo "Description: ${Description}"
 echo "Scenario: ${SCENARIO}"
 echo "Activity: ${Activity}"
-
